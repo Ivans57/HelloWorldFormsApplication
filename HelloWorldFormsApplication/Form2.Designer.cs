3e031aefa45fb46146f574145da6251fe666dd1b
Ivans Kunovs <ivans57@gmail.com>
2017-02-14 21:32:01 +0000

    Form application improved from laptop

--- HelloWorldFormsApplication/Form2.Designer.cs
+++ HelloWorldFormsApplication/Form2.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HelloWorldFormsApplication
 {
-    partial class Form1
+    partial class Form2
     {
         /// <summary>
         /// Required designer variable.
@@ -28,34 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // label1
             // 
-            this.button1.Location = new System.Drawing.Point(101, 84);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Press Me!";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(77, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hello!";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // Form1
+            // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(280, 213);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Press me";
+            this.ClientSize = new System.Drawing.Size(209, 107);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Hello";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
-
--- HelloWorldFormsApplication/Form2.cs
+++ HelloWorldFormsApplication/Form2.cs
@@ -10,17 +10,15 @@ using System.Windows.Forms;
 
 namespace HelloWorldFormsApplication
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
-            Form2 hello = new Form2();
-            hello.Show();
 
         }
     }
